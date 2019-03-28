767150258e24d6551166407493e3733bf585a08e
Kulicheg <41168303+Kulicheg@users.noreply.github.com>
2019-03-28 15:04:47 +0300

    Update ESP32_VGA_CLIENT.ino

--- ESP32_VGA_CLIENT.ino
+++ ESP32_VGA_CLIENT.ino
@@ -20,8 +20,7 @@ byte Packet[PackSize];
 byte currentByte;
 byte header [4] = {49, 50, 51, 52}; // 1234
 byte command;
-
-
+byte flag;
 
 void setup()
 {
@@ -31,9 +30,20 @@ void setup()
 
 void loop()
 {
-  fillrect(random(280)+20, random(160)+20, random(19), random(19), random(32768));
-  dot(160, 100, random(32768));
+
+  int c = 0;
+  for (int y = 0; y < 20; y++)
+  {
+    for (int x = 0; x < 30; x++)
+    {
+      rect (x * 10, y * 10, 10, 10, 65535);
+      c++;
+    }
+  }
+  // delay(1500);
+  cls(0);
 }
+
 
 
 void cls(uint16_t c)
@@ -110,7 +120,9 @@ void rect(int16_t x1, int16_t y1, int16_t w, int16_t h, uint16_t c)
   Pack.x2 = w;
   Pack.y2 = h;
   Pack.color = c;
+
   packSender (7);
+
 }
 
 void fillrect(int16_t x1, int16_t y1, int16_t w, int16_t h, uint16_t c)
@@ -181,11 +193,15 @@ void lprint(String text)
 
 void packSender (byte command)
 {
+byte answer = 0;
 
+while (answer !=32)
+  {
   for (byte q = 0; q < 4; q++)
   {
     Serial.write (header[q]);
   }
+
   Serial.write (command);
 
   unsigned char * Packet;
@@ -195,7 +211,43 @@ void packSender (byte command)
   {
     Serial.write(Packet[count]);
   }
+ 
 
-
-
+delay (2);
+  answer =  headerdetector();
+  Serial.println ("");
+  Serial.print ("answer = ");
+  Serial.println (answer);
+  }
 }
+
+byte headerdetector()
+{
+
+  byte command = 0;
+  byte flag = 0;
+  byte currentByte;
+
+
+  if (Serial.available())
+  {
+    while (flag < 4)
+    { currentByte = Serial.read();
+
+      if (currentByte == header [flag]) flag++; else flag = 0;
+
+    }
+  }
+
+  if (Serial.available()) command = Serial.read();
+
+  if (command > 32)
+  {
+    Serial.print ("Invalid command: ");
+    Serial.println (command);
+    command = 255;
+  }
+
+  return command;
+}
+
